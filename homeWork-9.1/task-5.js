20ad825773cba0b2ee908cd1726321d6502ea76c
AleksMoskalenko <aleks.moskalenko.work@gmail.com>
2024-11-22 15:38:39 +0200

    complete home work 9.1

---
+++ homeWork-9.1/task-5.js
@@ -0,0 +1,26 @@
+// Завдання 5
+// Створіть масив об'єктів users де обєкти мають довільні властивості (наприклад, name, email, age, тощо).
+// Використовуючи цикл for...of, переберіть всі елементи масиву та виведіть їхні значення в консоль.
+// Зробіть деструктуризацію в циклі
+
+const users = [
+  {
+    name: 'Kate',
+    email: 'kate@gmail.com',
+    age: 34,
+  },
+  {
+    name: 'Nick',
+    email: 'nick@gmail.com',
+    age: 46,
+  },
+  {
+    name: 'Alfred',
+    email: 'alfred@gmail.com',
+    age: 65,
+  },
+];
+
+for (const { name, email, age } of users) {
+  console.log(`Name: ${name}, Email: ${email}, Age: ${age}`);
+}
